0ee5471c223fbb117cc1e1489fdba6028b4d4394
varun-chaudhary555 <varunchaudhary210@gmail.com>
2026-03-25 22:14:31 +0530

    Update server.js

--- ai-flow-backend/server.js
+++ ai-flow-backend/server.js
@@ -4,7 +4,9 @@ const axios = require("axios");
 const mongoose =require("mongoose")
 require("dotenv").config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/ai-flow")
+const MONGO_URI = process.env.MONGO_URI
+
+mongoose.connect(MONGO_URI)
 .then(()=>{console.log("Database is connected........")})
 .catch((err)=>{console.log(err)})
 
